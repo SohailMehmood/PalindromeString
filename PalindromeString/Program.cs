a4a3510b4cadf9a8ff3a6b84f5479abf6fcfd516
Sohail Mehmood <sohail.mehmood@worldline.com>
2022-09-07 16:37:55 +0100

    Add project files.

---
+++ PalindromeString/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromeString;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args == null)
+        {
+            throw new ArgumentNullException(nameof(args));
+        }
+
+        Console.WriteLine("Enter a string : ");
+        
+        string input = Console.ReadLine() ?? string.Empty;
+
+        char[] firstString = input.ToCharArray();
+
+        Console.WriteLine(firstString);
+
+        Array.Reverse(firstString);
+
+        string reverseString = new(firstString);
+
+        Console.WriteLine(reverseString);
+
+        if (input == reverseString)
+        {
+            Console.WriteLine("This word is a Palindrome!");
+        }
+
+        else
+        {
+            Console.WriteLine("This word is not a Palindrome!");
+        }
+    }
+}
