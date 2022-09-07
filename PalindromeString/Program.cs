6ed94bc0a17e5e2a98db10a74ea7aee4b873073f
Sohail Mehmood <sohail.mehmood@worldline.com>
2022-09-07 17:47:00 +0100

    Cleaned up console output write lines to show the different variables

--- PalindromeString/Program.cs
+++ PalindromeString/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace PalindromeString;
 
@@ -13,27 +12,23 @@ public static class Program
         }
 
         Console.WriteLine("Enter a string : ");
-        
+
         string input = Console.ReadLine() ?? string.Empty;
 
         char[] firstString = input.ToCharArray();
-
-        Console.WriteLine(firstString);
 
         Array.Reverse(firstString);
 
         string reverseString = new(firstString);
 
-        Console.WriteLine(reverseString);
-
         if (input == reverseString)
         {
-            Console.WriteLine("This word is a Palindrome!");
+            Console.WriteLine($"{input} reversed is {reverseString}: This word is a Palindrome!");
         }
 
         else
         {
-            Console.WriteLine("This word is not a Palindrome!");
+            Console.WriteLine($"{input} reversed is {reverseString}: This word is not a Palindrome!");
         }
     }
 }
